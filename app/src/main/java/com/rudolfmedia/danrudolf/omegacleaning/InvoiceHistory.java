e54037106b875a922ddb0336bf947fa129aa7008
Dan Rudolf <danrudolf@gmail.com>
2014-08-16 12:56:25 -0400

    set up parse adapter

--- app/src/main/java/com/rudolfmedia/danrudolf/omegacleaning/InvoiceHistory.java
+++ app/src/main/java/com/rudolfmedia/danrudolf/omegacleaning/InvoiceHistory.java
@@ -8,39 +8,33 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 
+import com.parse.ParseObject;
+import com.parse.ParseQueryAdapter;
+
 
 public class InvoiceHistory extends Fragment {
 
-
+	private ParseQueryAdapter<ParseObject> invoiceAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-		final String[] values = new String[] { "Android List View",
-				"Adapter implementation",
-				"Simple List View In Android",
-				"Create List View Android",
-				"Android Example",
-				"List View Source Code",
-				"List View Array Adapter",
-				"Android Example List View"
-		};
-
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        FrameLayout layout = (FrameLayout) inflater.inflate(R.layout.fragment_invoice_history, container, false);
+
+final   FrameLayout layout = (FrameLayout) inflater.inflate(R.layout.fragment_invoice_history, container, false);
 
 		ListView invoicesListView = (ListView) layout.findViewById(R.id.invoiceList);
 
-
+		invoiceAdapter = new ParseQueryAdapter<ParseObject>(getActivity(), "Invoice");
+		invoiceAdapter.setTextKey("Date");
+		invoicesListView.setAdapter(invoiceAdapter);
+		invoiceAdapter.loadObjects();
 
 		return layout;
     }
-
 
 }
