1e52da68043194d3bdb600c45a426bed1d972907
Dan Rudolf <danrudolf@gmail.com>
2014-09-02 15:21:46 -0400

    cleaned up the detail view class

--- app/src/main/java/com/rudolfmedia/danrudolf/omegacleaning/InvoicesFragment.java
+++ app/src/main/java/com/rudolfmedia/danrudolf/omegacleaning/InvoicesFragment.java
@@ -1,18 +1,14 @@
 package com.rudolfmedia.danrudolf.omegacleaning;
 
 import android.app.Fragment;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
+import android.widget.RelativeLayout;
 
 
 public class InvoicesFragment extends Fragment {
-
-    private OnFragmentInteractionListener mListener;
 
 
     public InvoicesFragment() {
@@ -29,27 +25,11 @@ public class InvoicesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_invoices, container, false);
-    }
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_invoices, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+		return layout;
     }
 
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
 
 }
